0ca115ac9a990f7b22345edfe36b8e0f8ce39c80
JaffarAman <amanjaffar50@gmail.com>
2024-12-12 23:21:46 +0500

    javascript variables

---
+++ Module 2/class 02/app.js
@@ -0,0 +1,67 @@
+// alert("Hello Class");
+// alert("Hello World")
+
+// Variables
+// Jaffar Aman
+// SMIT
+// Pakistan
+
+
+
+// var , let , const
+
+// var fullname = "Jaffar Aman";
+// alert(fullname)
+
+// 10
+
+// var num1 = 10
+
+// alert(num1)
+
+// SMIT
+
+// JavaScript - Data types
+// 1. string = "jaffar " | 'jaffar '    Jaffar@123 => jaffar = " " | ' '
+// 2. number => 40 , 40.50 
+// 3. boolean
+// 4. null
+// 5. undefined
+
+// var value = "SMIT" //string Data type  
+// var value = 'SMIT' //string Data type  
+// var num1 = 500 // Number Data type
+// var num2 = 500.50 //Number Data type
+
+
+
+
+
+// var value = "SMIT";
+// var value = "SAYLANI"
+
+// console.log(value)
+
+// var myname = "Jaffar"
+// // alert(value)
+
+// myname = "Jaffar Aman"
+// console.log(myname)
+
+
+
+
+// var smitValue = "smit"
+
+
+
+
+
+// var age = 40
+
+
+var result;
+
+result = "A+"
+
+console.log(result)
