e0a020168304acbfcb3f59e48abeff355103978f
aiwithqasim <qasimhassan1020@gmail.com>
2024-12-17 23:17:13 +0500

    if statement

---
+++ Module 2/class 04/app.js
@@ -0,0 +1,111 @@
+// console.log('Hello class')
+
+// +-*/
+
+// var a = 5;
+// a = a + 1
+// console.log("a==>", a);
+
+// var b = 10;
+// // b = b + 1;
+// b += 1;
+// console.log(b);
+
+// var fullName = "jaffar";
+// fullName += " " + "Aman";
+// console.log("fullName", fullName);
+
+// var b = 10;
+// // b = b + 1;
+// // b += 1;
+
+// // b++;
+// // b++;
+// b--;
+// console.log("b=>", b);
+
+// increment or decrement operator.
+// var a = 10;
+// // a++; //Post increment
+// ++a; //pre increment
+// console.log(a);
+
+// a++ print phr add
+// ++a add phr print
+// var a = 20;
+// console.log(++a);
+// console.log(a);
+
+// console.log(a++ , a);
+// console.log(a++);
+// console.log(a)
+
+// var a = 50;
+// console.log(a++  , a);
+// console.log(++a  , a);
+
+var a = 5; //6  /7
+// var result = a++ + a + ++a
+//   5  +  6 + 7
+// a=5  /6 /7 / 8 /9
+// var result = ++a + ++a + a + a++ + a++;
+//    6  +  7  +  7 +  7 +  8
+
+// 5 / 6 / 7 /6 / 5
+// var result = a++ - ++a + a + a-- + --a + a
+//         //   5    - 7 +  7  + 7  + 5  + 5
+//         //   5    - 7 +  7  + 7  + 5  + 5
+
+// a = 10; //9 //8 //7 //8 //9 /8 /9 /8
+// var result = --a + --a + a-- + a + a++ + ++a - --a + a++ - --a;
+//   9   +  8   +  7  + 7  + 9
+// console.log("result", result);
+
+var a = 8;
+// abhi krna hai yeh kam class main 👏
+//1. a++ + ++a + --a - a--
+//2. a-- - a-- - ++a + a++ + a + --a
+//3. a++ + a++ + ++a - --a - ++a + a++ + --a
+
+// console.log(a++ + ++a + --a - a--);
+// console.log(a-- - a-- - ++a + a++ + a + --a);
+// console.log(a++ + a++ + ++a - --a - ++a + a++ + --a);
+
+// var num1 = 20;
+// var num2 = 40;
+// console.log(num1 + num2);
+
+// var  userName = prompt("Enter your name");
+// console.log("userName" , userName)
+
+// var num1 = +prompt("enter your num1");
+// var num2 = +prompt("enter your num2");
+// console.log(num1 + num2);
+
+// var name = prompt("Enter your name", "best Practices");
+
+// condition => if / else
+
+// // var age = 18
+// console.log("ALLOW")
+// console.log("NOT ALLOW")
+
+// = ==> assigning opt
+// == ==> equal to
+// === ==> equal to
+
+var age = 18;
+if (age == 18) {
+  console.log("Allow");
+} else {
+  console.log("not Allow");
+}
+
+console.log(age);
+
+// if (condition) {
+//
+// }
+// else{
+
+// }
