72f713269c86b30fb42392290d8e272fbc70c4ab
JaffarAman <amanjaffar50@gmail.com>
2025-03-11 08:18:46 +0500

    the dom start

---
+++ Module 2/class 34/app.js
@@ -0,0 +1,8 @@
+// console.log(document.childNodes[1].childNodes);
+
+// console.log(document.childNodes[1].childNodes[2].childNodes[1].childNodes);
+// var h1Element = document.childNodes[1].childNodes[2].childNodes[1];
+// h1Element.style.color = "red";
+// console.log("h1Element", h1Element);
+
+console.log(document.childNodes[1].childNodes);
