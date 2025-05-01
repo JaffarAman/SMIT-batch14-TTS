0b0a84912ecb4a1965ac6c0b2d92d22040038396
JaffarAman <amanjaffar50@gmail.com>
2025-05-01 22:52:02 +0500

    localstorage

---
+++ Module 2/class 51/app.js
@@ -0,0 +1,48 @@
+// function showValue() {
+//   //   var input = document.getElementById("input");
+//   //   var userName = document.getElementById("userName");
+
+//   //   userName.innerHTML = input.value;
+
+//   localStorage.setItem("heading", input.value);
+//   getValue()
+// }
+
+// function getValue() {
+//   var value = localStorage.getItem("heading");
+//   console.log("value", value);
+//   var userName = document.getElementById("userName");
+
+//   userName.innerHTML = value;
+// }
+
+// localStorage.setItem("bool", true);
+
+// var num = localStorage.getItem("bool");
+// console.log("num", typeof num);
+
+// var arr = ["JAFFAR", "sufiyan"];
+// var arr = {
+//   a: 10,
+//   b: 20,
+// };
+// console.log(arr);
+
+// localStorage.setItem("obj", arr);
+
+// var getArr = localStorage.getItem("obj");
+// console.log("getArr", getArr);
+
+// SAVE VALUEs
+// var arr = ["JAFFAR", "AMAN"];
+// var str = JSON.stringify(arr);
+// console.log("str", typeof str);
+// localStorage.setItem("userNames", str);
+
+// GET VALUES
+// var userArr = localStorage.getItem("userNames");
+// var parseArr = JSON.parse(userArr);
+// console.log("userArr", userArr);
+// console.log("parseArr", parseArr);
+
+// sessionStorage.setItem("value", "shapatar");
