f54b9352801ddc1698cee5f9d6895c9b92fa2fa1
JaffarAman <amanjaffar50@gmail.com>
2025-09-14 22:42:59 +0500

    backend start

---
+++ Backend/class 01/app.js
@@ -0,0 +1,29 @@
+// const num1 = 10;
+// const num2 = 20;
+// console.log("result=>>>>" ,num1 + num2)
+
+// alert("HELLO WORLD")
+
+// fs => module
+
+import fs from "fs";
+
+// create a file using fs
+// fs.writeFileSync("fileName", "file Content");
+// fs.writeFileSync("text.txt", "HELLO WORLD 1...");
+
+// read file using fs
+// const data = fs.readFileSync("text.txt" , "utf-8")
+// console.log(data)
+
+// update file
+// fs.appendFileSync("text.txt", "\n Hello world 5");
+
+// delete file
+// fs.unlinkSync("text.txt")
+
+// fs.mkdirSync("data/index.js", { recursive: true });
+
+// import { add } from "./utils.js";
+
+// console.log("add", add);
